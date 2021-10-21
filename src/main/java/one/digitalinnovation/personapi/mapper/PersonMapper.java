047f9099d072d7025cc91a9abb7bed8f41d7af98
Felipe Evangelista Alves <77590921+felipe-alves142@users.noreply.github.com>
2021-10-21 10:30:53 -0300

    Update PersonMapper.java

--- src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
+++ src/main/java/one/digitalinnovation/personapi/mapper/PersonMapper.java
@@ -4,10 +4,11 @@ import one.digitalinnovation.personapi.dto.request.PersonDTO;
 import one.digitalinnovation.personapi.entity.Person;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
 import org.mapstruct.factory.Mappers;
 
 
-@Mapper(componentModel = "spring")
+@Mapper(componentModel = "spring",unmappedTargetPolicy = ReportingPolicy.IGNORE)
 public interface PersonMapper {
     PersonMapper INSTANCE = Mappers.getMapper(PersonMapper.class);
 
